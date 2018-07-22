bb6300571d02bff6139f6a75887677e7b34a41cc
Francisco Arcos Jaiems <farcosj51@gmail.com>
2018-07-22 15:21:35 -0500

    Mi tercer commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Hola mundo react</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Esto lo hago para practicar git 
         </p>
       </div>
     );
